296d549169d0d545ed37052f127cb812bc76c236
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2024-11-11 10:03:35 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  let quien = ["Mi tia", "Mi hermana", "Mi gato", "El vecino"];
+  let accion = ["robo", "perdio", "rompio", "tiro"];
+  let que = ["mi llave", "mi telefono", "mi ordenador"];
+  let cuando = ["ayer", "hoy por la mañana", "por la tarde", "el mes pasado"];
+
+  function generarExcusa() {
+    let quienAleatorio = quien[Math.floor(Math.random() * quien.length)];
+    let accionAleatoria = accion[Math.floor(Math.random() * accion.length)];
+    let queAleatorio = que[Math.floor(Math.random() * que.length)];
+    let cuandoAleatorio = cuando[Math.floor(Math.random() * cuando.length)];
+
+    return `${quienAleatorio} ${accionAleatoria} ${queAleatorio} ${cuandoAleatorio}.`;
+  }
+  document.getElementById("excuse").innerText = generarExcusa();
+};
+
+document.getElementById("refreshButton").addEventListener("click", function() {
+  location.reload();
+});
